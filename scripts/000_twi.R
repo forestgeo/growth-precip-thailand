5115ded809baf2ab8e9a5c81fb285c87bb7c531f
Krishna Anujan <ka2602@columbia.edu>
2025-09-04 13:03:52 -0400

    updated SI doc with new figs

--- scripts/000_twi.R
+++ scripts/000_twi.R
@@ -128,7 +128,7 @@ tm_shape(twi) +
   tm_ylab("Grids North") +
   tm_grid(lines = F)
 
-png("doc/display/explore/twi.png", width = 4, height = 8, units = "in", res = 300)
+png("doc/display/twi.png", width = 4, height = 8, units = "in", res = 300)
 tm_shape(twi) +
   tm_raster(style = "cont", palette = "PuOr", legend.show = TRUE, alpha = 0.5) +
   tm_scale_bar() +
